c2b0828516236c43b39251029b585063d075f3a0
Thomas Buß <tbuss93@gmail.com>
2017-12-24 11:03:11 +0100

    Config application and supervisor

--- lib/plugtutorial.ex
+++ lib/plugtutorial.ex
@@ -1,5 +1,18 @@
 defmodule Plugtutorial do
-  @moduledoc """
-  Documentation for Plugtutorial.
-  """
+  use Application
+  require Logger
+
+  def start(_type, _args) do
+    children = [
+      Plug.Adapters.Cowboy.child_spec(
+        :http,
+        Example.HelloWorldPlug,
+        [],
+        port: 8080)
+    ]
+
+    Logger.info("Started application")
+
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
 end
