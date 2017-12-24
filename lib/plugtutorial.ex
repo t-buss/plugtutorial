f1f7cb47fe7442e96f5e9d731c310e5706ec9449
Thomas Buß <tbuss93@gmail.com>
2017-12-24 10:41:56 +0100

    Delete hello function from root module

--- lib/plugtutorial.ex
+++ lib/plugtutorial.ex
@@ -2,17 +2,4 @@ defmodule Plugtutorial do
   @moduledoc """
   Documentation for Plugtutorial.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Plugtutorial.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
